8ef78a50b94a1f7dfd57fe18eff9456bbc915ed6
WangleyQuinhones <wangleyquinhones@gmail.com>
2024-01-10 18:12:28 -0400

    Update main.py

--- main.py
+++ main.py
@@ -30,6 +30,33 @@ app_linha = Label(frame_cima, text='', width=400, height=1, padx=0, relief='flat
                   anchor='center', font=('Ivy 1'), bg=co2, fg=co1)
 app_linha.place(x=0, y=35)
 
+# Função Calculos
+
+def calcular():
+
+  peso = float(e_peso.get())
+  altura = float(e_altura.get())
+
+  imc = peso / altura**2
+  resultado = imc
+
+  if resultado < 18.5:
+    l_resultado_texto['text'] = "Seu IMC está: Abaixo do peso!",
+
+  elif resultado >= 18.5 and resultado < 25:
+    l_resultado_texto['text'] = "Seu IMC está: No peso ideal!",
+
+  elif resultado >= 25 and resultado < 30:
+    l_resultado_texto['text'] = "Seu IMC está: Acima do peso!",
+
+  else:
+    l_resultado_texto['text'] = "Seu IMC está: Obesidade!",
+
+    
+    l_resultado['text'] = "{:.{}f}".format(resultado, 2)
+  
+
+
 # configurando frane baixo
 
 l_peso = Label(frame_baixo, text='Insira seu peso', height=1, padx=0, relief='flat',anchor='center',
@@ -48,32 +75,13 @@ l_resultado = Label(frame_baixo, text='---', width=5, height=1, padx=6, pady=12,
                 font=('Ivy 24 bold'), bg=co2, fg=co0)
 l_resultado.place(x=175, y=10)
 
-l_resultado_texto = Label(frame_baixo, text='O seu IMC é: você está abaixo do peso!', width=37, height=1, padx=0, pady=12, relief='flat',anchor='center',
+l_resultado_texto = Label(frame_baixo, text='', width=37, height=1, padx=0, pady=12, relief='flat',anchor='center',
                 font=('Ivy 10 bold'), bg=co0, fg=co1)
 l_resultado_texto.place(x=0, y=90)
 
-b_calcular = Button(frame_baixo, text='Calcular', width=37, height=1, overrelief=SOLID, relief='raised',anchor='center',
+b_calcular = Button(frame_baixo, command=calcular, text='Calcular', width=37, height=1, overrelief=SOLID, relief='raised',anchor='center',
                 font=('Ivy 10 bold'), bg=co2, fg=co0)
 b_calcular.grid(row=4, column=0, sticky=NSEW, pady=60, padx=5, columnspan=30)
 
-# Calculos
-
-peso=100
-altura=1.76
-resultado=peso/altura
-
-if resultado < 18.5:
-    print('Seu IMC está: Abaixo do peso!')
-elif resultado >= 18.5  and resultado < 25:
-    print('Seu IMC está: No peso ideal!')
-elif resultado >= 25 and resultado < 30:
-    print('Seu IMC está: Acima do peso!')
-else:
-    print('Seu IMC está: Obesidade!')
-  
-
-resultado="{:.{}f}".format(resultado, 2)
-
-print(resultado)  
 
 janela.mainloop()
